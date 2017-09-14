1854a7e184d7e34b6ff16ea74caf87414844acf8
HenanXiao4 <wingrakhi@live.jp>
2017-09-15 01:18:42 +0900

    progress viewmodel

---
+++ Assets/Scripts/ViewModel/MainCatViewModel.cs
@@ -0,0 +1,34 @@
+﻿using Interface;
+using UniRx;
+using DateTime = System.DateTime;
+using TimeSpan = System.TimeSpan;
+
+namespace ViewModel
+{
+    public class MainCatViewModel : IMainCatViewModel
+    {
+        public IObservable<int> remainHp
+        {
+            get { return remainHpViewModel; }
+        }
+
+        private readonly ReactiveProperty<int> remainHpViewModel = new ReactiveProperty<int>(50000);
+
+        public IObservable<TimeSpan> remainTime
+        {
+            get { return remainTimeViewModel; }
+        }
+
+        private readonly ReactiveProperty<TimeSpan> remainTimeViewModel =
+            new ReactiveProperty<TimeSpan>(DateTime.Now - DateTime.Today);
+
+        public void OnCLickAttackButton(float damage)
+        {
+            remainHpViewModel.Value = (int)(damage * 1000);
+        }
+
+        public MainCatViewModel()
+        {
+        }
+    }
+}
