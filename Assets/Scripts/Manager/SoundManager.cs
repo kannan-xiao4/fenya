f17b16eb043477d669d268e9f6cf9cfb550065ad
HenanXiao4 <wingrakhi@live.jp>
2017-09-15 01:14:59 +0900

    project setting 1

---
+++ Assets/Scripts/Manager/SoundManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using Utility;
+
+namespace Manager
+{
+    /// <summary>
+    /// Manage Sound Play
+    /// </summary>
+    public class SoundManager : SingletonMonoBehaviour<SoundManager>
+    {
+        [SerializeField]
+        private AudioSource audioSource;
+
+        [SerializeField]
+        private List<AudioClip> nyaClipList;
+
+        /// <summary>
+        /// Play Type of Nya Sound
+        /// </summary>
+        /// <param name="type"></param>
+        public void PlayCommonNyaSound(NyaSoundType type)
+        {
+            var nyaClip = nyaClipList[(int) type];
+            PlayAudioClip(nyaClip);
+        }
+        
+        /// <summary>
+        /// Play AudioClip OneShot
+        /// </summary>
+        /// <param name="audio"></param>
+        public void PlayAudioClip(AudioClip audio)
+        {
+            audioSource.PlayOneShot(audio);
+        }
+    }
+}
---
+++ Assets/Scripts/Utility/SingletonMonoBehaviour.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+namespace Utility
+{
+    /// <summary>
+    /// Singleton of MonoBehaviour
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class SingletonMonoBehaviour<T> : MonoBehaviour where T : SingletonMonoBehaviour<T>
+    {
+        protected static T instance;
+
+        public static T Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = (T) FindObjectOfType(typeof(T));
+
+                    if (instance == null)
+                    {
+                        Debug.LogWarning(typeof(T) + "is nothing");
+                    }
+                }
+
+                return instance;
+            }
+        }
+
+        protected void Awake()
+        {
+            CheckInstance();
+        }
+
+        /// <summary>
+        /// Check for Exsist Instance
+        /// </summary>
+        /// <returns></returns>
+        protected bool CheckInstance()
+        {
+            if (instance == null)
+            {
+                instance = (T) this;
+                return true;
+            }
+            if (Instance == this)
+            {
+                return true;
+            }
+
+            Destroy(this);
+            return false;
+        }
+    }
+}
