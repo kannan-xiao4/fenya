2da0fe9dac10a13361c25cbc768c9289d7bfbef4
HenanXiao4 <wingrakhi@live.jp>
2017-09-16 15:04:34 +0900

    UIマネージャとPageSettingを作成

---
+++ Assets/Scripts/PageSettings/PageSetting.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+namespace PageSettings
+{
+    public abstract class PageSetting : ScriptableObject
+    {
+        [SerializeField]
+        protected GameObject pagePrefab;
+
+        private GameObject pageInstance;
+
+        public GameObject PageInstance
+        {
+            get { return pageInstance; }
+        }
+
+        /// <summary>
+        /// ページPrefabをインスタンス化します
+        /// </summary>
+        /// <param name="parent"></param>
+        public void InstantiatePage(Transform parent)
+        {
+            pageInstance = Instantiate(pagePrefab, parent);
+        }
+
+        /// <summary>
+        /// 現在のPageのViewを取得します
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public T GetPageView<T>()
+        {
+            return pageInstance.GetComponent<T>();
+        }
+
+        /// <summary>
+        /// 読み込んだモデルを遷移先ページに渡します。
+        /// </summary>
+        public abstract void BindLoadedModels();
+    }
+}
