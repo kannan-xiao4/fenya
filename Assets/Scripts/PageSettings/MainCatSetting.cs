ad6eb98731ff39b3592bcf08abe6d69df530525b
HenanXiao4 <wingrakhi@live.jp>
2017-09-16 15:04:53 +0900

    MainCatのPageSettingを作成

---
+++ Assets/Scripts/PageSettings/MainCatSetting.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace PageSettings
+{
+    [CreateAssetMenu]
+    public class MainCatSetting : PageSetting
+    {
+        public override void BindLoadedModels()
+        {
+        }
+    }
+}
