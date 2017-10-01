c98502b96f7801d28da0050afda3b3c4e7c5fd24
HenanXiao4 <wingrakhi@live.jp>
2017-10-01 17:03:49 +0900

    タイトル画面とそのモデルを実装

---
+++ Assets/Scripts/PageSettings/TitleSetting.cs
@@ -0,0 +1,35 @@
+﻿using Interface;
+using UnityEngine;
+using View;
+
+namespace PageSettings
+{
+    /// <summary>
+    /// タイトル画面のセッティング
+    /// </summary>
+    [CreateAssetMenu]
+    public class TitleSetting : PageSetting
+    {
+        /// <summary>
+        /// ViewModelを保持
+        /// </summary>
+        private ITitleViewModel viewModel;
+
+        /// <summary>
+        /// ViewにViewModelをBind
+        /// </summary>
+        internal override void BindLoadModel()
+        {
+            Instance.GetComponent<TitleView>().Bind(viewModel);
+        }
+
+        /// <summary>
+        /// SettingにViewModelをBind
+        /// </summary>
+        /// <param name="viewModel"></param>
+        public void Bind(ITitleViewModel viewModel)
+        {
+            this.viewModel = viewModel;
+        }
+    }
+}
